f2be220fecda4e569c54b91ea0b3a752bb1ad7b8
KevinFromConn <kfc21194@gmail.com>
2021-10-20 19:09:34 -0400

    formatted code and fixed a typo in typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,51 +1,44 @@
-const { User } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
-                    .select('-__v -password');
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
 
-                return userData;
-            }
+        return userData;
+      }
 
-            throw new AuthenticationError('Not logged in');
-        }
+      throw new AuthenticationError("Not logged in");
     },
+  },
 
-    Mutation: {
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-            if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
-            const correctPw = await user.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password);
 
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
-            const token = signToken(user);
-            return { token, user };
-        },
-        addUser: async () => {
-
-        },
-        saveBet: async () => {
-
-        },
-        updateBet: async () => {
-
-        },
-        removeAccount: async () => {
-
-        },
-    }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async () => {},
+    saveBet: async () => {},
+    updateBet: async () => {},
+    removeAccount: async () => {},
+  },
 };
 
 module.exports = resolvers;
