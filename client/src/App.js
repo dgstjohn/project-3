0a61e450f9a24780770484aa355b62a635339344
dgstjohn <83672340+dgstjohn@users.noreply.github.com>
2021-10-25 19:25:41 -0400

    Revert "Rich/feature"

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import logo from './logo.svg';
+import './App.css';
 
-const httpLink = createHttpLink({
-    uri: 'http://localhost:3001/graphql',
-  });
-  
-  const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-  });
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,32 +1,14 @@
 const express = require('express');
-const path = require('path');
 const db = require('./config/connection');
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
-
-server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`)
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  }
-)});
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
