1ad6c61f337ed891b849709dc6a9d9bbb0006937
dgstjohn <dgstjohn@gmail.com>
2021-10-24 21:30:53 -0400

    API test file done, package.json with dotenv updated

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,14 @@
 // temporary file to hold fetch calls for the ODDS API 
 // until we can insert it into a page function
 
-import dotenv from 'dotenv';
+import { dotenv } from 'dotenv';
+
+dotenv.config();
 
 export const searchSport = (query) => {
-    return fetch(`https://api.the-odds-api.com/v4/sports?apiKey=YOUR_API_KEY(opens new window)`);
-  };
+  return fetch(process.env.SEARCH_SPORT);
+};
+
+export const searchOdds = (query) => {
+  return fetch(process.env.SEARCH_ODDS);
+}
