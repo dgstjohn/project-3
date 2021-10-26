19bb4a0591b88b5db255b92c957a3e3647839efa
ridalo <ridalo1989@gmail.com>
2021-10-26 01:11:05 -0400

    Put some html together for the login and css, install express files to client for apollo and api.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import Login from "./components/login.comp";
+import SignUp from "./components/signup.comp";
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:3001/graphql',
@@ -9,4 +14,37 @@ const httpLink = createHttpLink({
     cache: new InMemoryCache(),
   });
 
-export default App;
+  
+  function App() {
+    return (<Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/sign-in"}>positronX.io</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+  
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+            </Switch>
+          </div>
+        </div>
+      </div></Router>
+    );
+  }
+  
+  export default App;
