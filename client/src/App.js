552247b232ec37ab4bb5e9c297804608f33569b4
KevinFromConn <kfc21194@gmail.com>
2021-10-25 20:09:26 -0400

    set up react to launch app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,48 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import Hamburger from "./pages/Hamburger";
+import Homepage from "./pages/HomePage";
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:3001/graphql',
-  });
-  
-  const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-  });
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Switch>
+            <Route exact path="/" component={Hamburger} />
+            <Route exact path="/homepage" component={Homepage} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
 
 export default App;
