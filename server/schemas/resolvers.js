7fe03146ec3826124d6cccb3f737f2c1287761ca
dgstjohn <dgstjohn@gmail.com>
2021-11-01 01:26:11 -0400

    commented out Stripe-related code in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { User, Team, Order } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
-const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+// const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
 const resolvers = {
   Query: {
@@ -20,45 +20,43 @@ const resolvers = {
     teams: async (parent, args) => {
       return Team.find();
     },
-
-    checkout: async (parent, args, context) => {
-      const order = new Order({ bets: args.bets });
-      const { bets } = await order.populate("bets").execPopulate();
-      const line_items = [];
-
-      for (let i = 0; i < bets.length; i++) {
-        // generate product id
-        const bets = await stripe.bets.create({
-          team: bets[i].team,
-          amount: bets[i].amount,
-        });
-
-        // generate price id using the product id
-        const price = await stripe.prices.create({
-          bet: bets.id,
-          unit_amount: bets[i].amount * 100,
-          currency: "usd",
-        });
-
-        // add price id to the line items array
-        line_items.push({
-          amount: amount.id,
-          quantity: 1,
-        });
-      }
-      // this code should be OK as is
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items,
-        mode: "payment",
-        success_url:
-          "https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url: "https://example.com/cancel",
-      });
-
-      return { session: session.id };
-    },
   },
+  //   checkout: async (parent, args, context) => {
+  //     const order = new Order({ bets: args.bets });
+  //     const { bets } = await order.populate("bets").execPopulate();
+  //     const line_items = [];
+
+  //     for (let i = 0; i < bets.length; i++) {
+  //       const bets = await stripe.bets.create({
+  //         team: bets[i].team,
+  //         amount: bets[i].amount,
+  //       });
+
+  //       // generate price id using the product id
+  //       const price = await stripe.prices.create({
+  //         bet: bets.id,
+  //         unit_amount: bets[i].amount * 100,
+  //         currency: "usd",
+  //       });
+
+  //       // add price id to the line items array
+  //       line_items.push({
+  //         amount: amount.id,
+  //         quantity: 1,
+  //       });
+  //     }
+  //     const session = await stripe.checkout.sessions.create({
+  //       payment_method_types: ["card"],
+  //       line_items,
+  //       mode: "payment",
+  //       success_url:
+  //         "https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
+  //       cancel_url: "https://example.com/cancel",
+  //     });
+
+  //     return { session: session.id };
+  //   },
+  // },
 
   Mutation: {
     login: async (parent, { email, password }) => {
